1f861134b8271b98fd720d8cbff3ebf488339123
Achintya <achintyashende@gmail.com>
2025-03-04 14:40:52 +0530

    structure

---
+++ src/server.js
@@ -0,0 +1,7 @@
+// console.log("Backend for ODIIGO");
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log("Listen on port " + port));
