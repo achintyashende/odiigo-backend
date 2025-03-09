4a696bd1d419198126afd1cf9811c8913e23d308
Achintya <achintyashende@gmail.com>
2025-03-09 22:16:10 +0530

    Done CRUD operation for Categories.

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000
 app.use(express.json());
 app.use("/api/vehicles", require('./services/vehicles/routes/vehicleRoutes'))
 app.use("/api/services", require('./services/categories/services/routes/serviceRoutes'))
+app.use("/api/categories", require('./services/categories/routes/categoryRoutes'))
 app.listen(port, () => {
     console.log(`Server is running at ${port}`)
 });
