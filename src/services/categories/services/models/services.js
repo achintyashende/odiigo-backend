f5ca93b32a7ebfa1fb5bd5da8adc6d7e251bd6d3
Achintya <achintyashende@gmail.com>
2025-03-12 14:25:42 +0530

    Done CRUD for service-pricing

--- src/services/categories/services/models/services.js
+++ src/services/categories/services/models/services.js
@@ -4,7 +4,7 @@ const serviceSchema = new mongoose.Schema({
     service_name: { type: String, required: true },
     service_details: [{ type: String, required: true }],
     included_services: [{ type: String, required: true }],
-    price: { type: Number, required: true },
+    // price: { type: Number, required: true },
     category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true }
 }, { timestamps: true });
 
