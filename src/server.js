c5dc09d639671068969d1d9dd9d6d593fefa079a
Achintya <achintyashende@gmail.com>
2025-03-18 14:58:01 +0530

    Renamed to odiigo-modules

--- src/server.js
+++ src/server.js
@@ -7,12 +7,12 @@ const app = express();
 const port = process.env.PORT || 5000
 
 app.use(express.json());
-app.use("/api/vehicles", require('./services/vehicles/routes/vehicleRoutes'))
-app.use("/api/services", require('./services/categories/services/routes/serviceRoutes'))
-app.use("/api/categories", require('./services/categories/routes/categoryRoutes'))
-app.use("/api/servicePricing", require('./services/service-prices/routes/servicePricingRoutes'))
-app.use("/api/userProfile", require('./services/users/routes/userRoutes'))
-app.use('/api/order', require('./services/orders/routes/orderRoutes'))
+app.use("/api/vehicles", require('./odiigo-modules/vehicles/routes/vehicleRoutes'))
+app.use("/api/services", require('./odiigo-modules/categories/services/routes/serviceRoutes'))
+app.use("/api/categories", require('./odiigo-modules/categories/routes/categoryRoutes'))
+app.use("/api/servicePricing", require('./odiigo-modules/service-prices/routes/servicePricingRoutes'))
+app.use("/api/userProfile", require('./odiigo-modules/users/routes/userRoutes'))
+app.use('/api/order', require('./odiigo-modules/orders/routes/orderRoutes'))
 app.listen(port, () => {
     console.log(`Server is running at ${port}`)
 });
